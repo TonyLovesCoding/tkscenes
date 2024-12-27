d105dfe42cd1dcb24927a9a839946bf86abb84b2
tonyh <tonyhyphens@gmail.com>
2024-12-27 22:04:52 +0700

    Idk what im doing

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+with codecs.open(os.path.join(here, "Readme.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'A package for adding scenes to TKinter and CustomTKinter'
 LONG_DESCRIPTION = 'A simple package for adding different scenes to TKinter and CustomTKinter'
 
 # Setting up
 setup(
-    name="TkScenes",
+    name="tkscenes",
     version=VERSION,
     author="TonyLovesCoding",
-    author_email="<tonyhyphens@gmail.com>",
+    author_email="<tony.tonylovescoding@gmail.com>",
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
     keywords=['scene', "simple"],
@@ -28,5 +30,8 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+    ],
+    project_urls={
+        "Source": "https://github.com/TonyLovesCoding/tkscenes/"
+    }
 )
